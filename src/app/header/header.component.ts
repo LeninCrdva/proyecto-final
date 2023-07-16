7e6e9ffb1651bbbc6f13c312fe8ec4082ff69fa2
Miguel13fdf <122501535+Miguel13fdf@users.noreply.github.com>
2023-07-16 18:11:43 -0500

    Redirecciones y Cambios de pie de pagina
    Poner redirecciones del panel del admin ademas arreglar los pies de pagina de la pagina principal y del login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataStorageService } from '../PerfilUsuarios/data-storage.service';
-
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,17 +12,35 @@ export class HeaderComponent {
 
   modalTitle:string = 'Crear Custodio';
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService
+    , private router: Router) {
     // Obtener los datos del usuario almacenados en las cookies
     this.usuario = this.dataStorageService.getData('datosUsuario'); // Reemplaza 'nombreDeTuLlave' con la llave que usaste para guardar los datos del usuario
   }
   get isPerfil(): boolean {
     return !!(this.usuario && this.usuario.primerNombre && this.usuario.apellidoPaterno);
   }
+  ngOnInit(): void {
+    // Verificar si el perfil está incompleto al iniciar el componente
+    if (!this.isPerfil) {
+      // Mostrar SweetAlert con el mensaje de error
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Tu perfil no está completo. Inicia sesión nuevamente.',
+        confirmButtonText: 'Aceptar'
+      }).then(() => {
+        // Redireccionar al usuario a la página de inicio de sesión (login)
+        // Aquí debes especificar la ruta correcta hacia la página de inicio de sesión
+         this.router.navigate(['/login']);
+      });
+    }
+  }
 
   // Método para cerrar sesión
   cerrarSesion() {
     // Eliminar los datos del usuario al cerrar sesión
     this.dataStorageService.deleteData('datosUsuario'); // Reemplaza 'nombreDeTuLlave' con la misma llave que usaste anteriormente
+    
   }
 }
