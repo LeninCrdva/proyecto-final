2c24c0be228160636598bb76e866e64062000b9f
Santiago G <santiagogcan@gmail.com>
2023-07-21 08:40:05 -0500

    Update Historial

--- src/app/entities/historial.ts
+++ src/app/entities/historial.ts
@@ -1,7 +1,10 @@
+import { Bien } from "./bien";
+import { Usuario } from "./usuario";
+
 export class Historial {
     'his_cod': number=0;
     'his_fecha': Date = new Date(); 
     'his_detalles': String="";
-    'his_usu_cod': number=0;
-    'his_bie_cod': number=0;
+    'usuario': Usuario;
+    'bien': Bien;
 }
